5a3b2ba43c882c0decb7faeeee6ce0e610b58e18
Zachary Ernst <zac.ernst@gmail.com>
2025-04-24 14:04:31 -0400

    enums file

---
+++ packages/nmetl/src/nmetl/session_enums.py
@@ -0,0 +1,21 @@
+"""Just some enums for initializers."""
+
+from enum import Enum
+
+
+class BackEndEnum(str, Enum):
+    """Enum for the back end to use for the session."""
+
+    THREADING = "threading"
+    MULTIPROCESSING = "multiprocessing"
+    DASK = "dask"
+
+
+class LoggingLevelEnum(str, Enum):
+    """Enum for the logging level to use for the session."""
+
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
