620679dd733e5378c3cdd3c6e81343819ac46e2e
Zachary Ernst <zac.ernst@gmail.com>
2025-12-30 14:03:35 +0000

    algebra v2

--- packages/pycypher/src/pycypher/algebra.py
+++ packages/pycypher/src/pycypher/algebra.py
@@ -3,10 +3,18 @@ from abc import ABC, abstractmethod
 from pydantic import field_validator, BaseModel
 from typing import Any, List, Optional
 from enum import Enum
+import copy
+import random
+import hashlib
+import rich
 import pandas as pd
 
 # ID_KEY: str = "__id__"
-VARIABLE_PREFIX: str = "__var__"
+# VARIABLE_PREFIX: str = "__var__"
+
+
+def random_hash() -> str:
+    return hashlib.md5(bytes(str(random.random()), encoding='utf-8')).hexdigest()
 
 
 class JoinType(str, Enum):
@@ -17,32 +25,46 @@ class JoinType(str, Enum):
 
 
 class Algebraic(BaseModel):
+    pass
 
-    @abstractmethod
-    def to_pandas(self, context: Context) -> pd.DataFrame:
-        raise NotImplementedError(
-            "Subclasses must implement to_pandas method"
-        )
+    # @abstractmethod
+    # def to_pandas(self, context: Context) -> pd.DataFrame:
+    #     raise NotImplementedError(
+    #         "Subclasses must implement to_pandas method"
+    #     )
+
 
 class Table(Algebraic):
-    name: str
+    identifier: str = ''
+    column_name_to_hash: dict[str, str] = {}
+    hash_to_column_name: dict[str, str] = {}
+
+    @field_validator("identifier", mode="after")
+    @classmethod
+    def set_identifier(cls, v: str) -> str:
+        if v == '':
+            return random_hash()
+        return v
 
 
 class EntityTable(Table):
     entity_type: str
     attributes: List[str]
-    identifier_column_name: str
+    entity_identifier_attribute: str
+    variables_to_columns: dict[str, str] = {}
 
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        for attribute in self.attributes:
+            column_hash: str = random_hash()
+            self.column_name_to_hash[attribute] = column_hash
+            self.hash_to_column_name[column_hash] = attribute
+    
     def to_pandas(self, context: Context) -> pd.DataFrame:
-        if self.entity_type not in context.obj_map:
-            raise ValueError(f"Entity type {self.entity_type} not found in context")
-        # Create __id__ column if not exists
-        df: pd.DataFrame = context.obj_map[self.entity_type]
-        if ID_KEY not in df.columns:
-            df = df.copy()
-            df[ID_KEY] = df[self.identifier_column_name]
-            context.obj_map[self.entity_type] = df
-        return context.obj_map[self.entity_type]
+        df: pd.DataFrame = context.obj_map[self.entity_type].rename(
+            mapper=self.column_name_to_hash, axis=1,
+        )
+        return df
 
 
 class RelationshipTable(Table):
@@ -50,13 +72,43 @@ class RelationshipTable(Table):
     source_entity_type: str
     target_entity_type: str
     attributes: List[str]
-    variable_list: List[str] = []
-    
-    def to_pandas(self, context: Context) -> pd.DataFrame:
-        if self.relationship_type not in context.obj_map:
-            raise ValueError(f"Relationship type {self.relationship_type} not found in context")
-        return context.obj_map[self.relationship_type]
+    relationship_identifier_attribute: str
 
+
+
+
+class Context(BaseModel):
+    entity_tables: List[EntityTable]
+    relationship_tables: List[RelationshipTable]
+    obj_map: dict[str, Any] = {}
+
+    def get_entity_table(self, entity_type: str) -> EntityTable:
+        for entity_table in self.entity_tables:
+            if entity_table.entity_type == entity_type:
+                return entity_table
+        else:
+            raise ValueError(f"Entity table for type {entity_type} not found")
+
+
+
+class Node(BaseModel):
+    variable: str
+    label: str
+    properties: dict = {}
+
+
+    def to_algebra(self, context: Context) -> EntityTable:
+        entity_table: Table = context.get_entity_table(self.label)
+        entity_table.variables_to_columns[self.variable] = entity_table.column_name_to_hash[entity_table.entity_identifier_attribute]
+        return entity_table
+    
+    def __str__(self) -> str:
+        return f"({self.label}:{self.variable})"
+
+
+
+
+    
 
 class ConvertableToAlgebra(ABC, BaseModel):
     @abstractmethod
@@ -98,53 +150,7 @@ class Filter(Algebraic):
     condition: str
 
 
-class Context(BaseModel):
-    entity_tables: List[EntityTable]
-    relationship_tables: List[RelationshipTable]
-    obj_map: dict[str, Any] = {}
 
-    def get_entity_table(self, entity_type: str) -> EntityTable:
-        for table in self.entity_tables:
-            if table.entity_type == entity_type:
-                return table
-        else:
-            raise Exception(
-                f"Entity type {entity_type} not found in entity tables"
-            )
-
-    def get_relationship_table(
-        self, relationship_type: str
-    ) -> RelationshipTable:
-        for table in self.relationship_tables:
-            if table.relationship_type == relationship_type:
-                return table
-        else:
-            raise Exception(
-                f"Relationship type {relationship_type} not found in relationship tables"
-            )
-
-
-class Node(ConvertableToAlgebra):
-    variable: str
-    label: str
-    properties: Optional[dict] = None
-    variable_list: list[str] = []
-
-
-    def to_algebra(self, context: Context) -> RenameColumn:
-        entity_table: Table = context.get_entity_table(self.label)
-        # variable_table = VariableTable(parent_node = self, table=entity_table, variable_list=[self.variable])
-        rename_column = RenameColumn(
-            name=entity_table.name,
-            table=entity_table,
-            source_column=context.get_entity_table(self.label).identifier_column_name,
-            target_column=f"{VARIABLE_PREFIX}{self.variable}",
-            variable_list=[self.variable]
-        )
-        return rename_column
-    
-    def __str__(self) -> str:
-        return f"({self.variable})"
 
 
 class Relationship(ConvertableToAlgebra):
@@ -161,19 +167,13 @@ class Relationship(ConvertableToAlgebra):
         left_join = Join(
             left=source_table,
             right=relationship_table,
-            left_on=f"{VARIABLE_PREFIX}{self.source_node.variable}",
-            right_on='source_node',
+            left_on=source_table.variables_to_columns[self.source_node.variable],
+            right_on=relationship_table.column_name_to_hash[relationship_table.source_entity_type],
             join_type=JoinType.INNER,
-        )
-        renamed_left_join: RenameColumn = RenameColumn(
-            table=left_join,
-            source_column=source_table.identifier_column_name,
-            target_column=f"{VARIABLE_PREFIX}{self.variable}",
-            variable_list=[self.variable] + left_join.variable_list,
         )
 
         right_join = Join(
-            left=renamed_left_join,
+            left=left_join,
             right=target_table,
             left_on='target_node',
             right_on=f"{VARIABLE_PREFIX}{self.target_node.variable}",
@@ -186,76 +186,79 @@ class Relationship(ConvertableToAlgebra):
         return f"({self.source_node.variable})-[:{self.label}]->({self.target_node.variable})"
 
 
-class RenameColumn(Table):
-    table: Table | Join
-    source_column: str
-    target_column: str
-    variable_list: list[str] = []
-    identifier_column_name: Optional[str] = None
-
-    @field_validator("identifier_column_name", mode="before")
-
-    def to_pandas(self, context: Context) -> pd.DataFrame:
-        df: pd.DataFrame = self.table.to_pandas(context)
-        df = df.rename(columns={self.source_column: self.target_column})
-        return df
-
-
 if __name__ == "__main__":
-    person_df: pd.DataFrame = pd.DataFrame(
-        data=[{"name": "Alice", "age": 30, "city": "Cairo"}], columns=["name", "age", "city"]
-    )
-
-    city_df: pd.DataFrame = pd.DataFrame(
-        data=[{"name": "Cairo", "population": 100}], columns=["name", "population"]
-    )
-
-    lives_in_df: pd.DataFrame = pd.DataFrame(
-        data=[{"source_name": "Alice", "target_name": "Cairo"}],
-    )
-
-    person_table = EntityTable(
+    entity_table: EntityTable = EntityTable(
         name="Person", entity_type="Person", attributes=["name", "age"],
-        identifier_column_name="name"
+        entity_identifier_attribute="name"
     )
-
-    city_table = EntityTable(
-        name="City", entity_type="City", attributes=["name", "population"],
-        identifier_column_name="name"
-    )
-
-    lives_in_table = RelationshipTable(
-        name="LIVES_IN",
-        relationship_type="LIVES_IN",
-        source_entity_type="Person",
-        target_entity_type="City",
-        attributes=[],
-        obj=lives_in_df,
-        variable_list=["r"],
-    )
-
-    person_node = Node(
+    person_node: Node = Node(
         variable="p", label="Person", properties={}
     )
-
-    city_node = Node(
-        variable="c", label="City", properties={}
+    person_df = pd.DataFrame(
+        data=[{"name": "Alice", "age": 30}, {"name": "Bob", "age": 25}], columns=["name", "age"]
     )
-
-    relationship = Relationship(
-        variable="r",
-        label="LIVES_IN",
-        source_node=person_node,
-        target_node=city_node,
+    context: Context = Context(
+        entity_tables=[entity_table], relationship_tables=[],
+        obj_map={"Person": person_df},
     )
-    context = Context(
-        entity_tables=[person_table, city_table], relationship_tables=[lives_in_table],
-        obj_map={"Person": person_df, "City": city_df, "LIVES_IN": lives_in_df},
-    )
-    # obj: Algebraic = relationship.to_algebra(context)
-    # print(obj)
-    
-    import rich
-    obj: Algebraic = relationship.to_algebra(context)
+    obj = person_node.to_algebra(context)
     rich.print(obj)
-    print(obj.to_pandas(context))
+    df = obj.to_pandas(context)
+    # person_df: pd.DataFrame = pd.DataFrame(
+    #     data=[{"name": "Alice", "age": 30, "city": "Cairo"}], columns=["name", "age", "city"]
+    # )
+
+    # city_df: pd.DataFrame = pd.DataFrame(
+    #     data=[{"name": "Cairo", "population": 100}], columns=["name", "population"]
+    # )
+
+    # lives_in_df: pd.DataFrame = pd.DataFrame(
+    #     data=[{"source_name": "Alice", "target_name": "Cairo"}],
+
+    # )
+
+    # person_table = EntityTable(
+    #     name="Person", entity_type="Person", attributes=["name", "age"],
+    #     identifier_column_name="name"
+    # )
+
+    # city_table = EntityTable(
+    #     name="City", entity_type="City", attributes=["name", "population"],
+    #     identifier_column_name="name"
+    # )
+
+    # lives_in_table = RelationshipTable(
+    #     name="LIVES_IN",
+    #     relationship_type="LIVES_IN",
+    #     source_entity_type="Person",
+    #     target_entity_type="City",
+    #     attributes=[],
+    #     obj=lives_in_df,
+    #     variable_list=["r"],
+    # )
+
+    # person_node = Node(
+    #     variable="p", label="Person", properties={}
+    # )
+
+    # city_node = Node(
+    #     variable="c", label="City", properties={}
+    # )
+
+    # relationship = Relationship(
+    #     variable="r",
+    #     label="LIVES_IN",
+    #     source_node=person_node,
+    #     target_node=city_node,
+    # )
+    # context = Context(
+    #     entity_tables=[person_table, city_table], relationship_tables=[lives_in_table],
+    #     obj_map={"Person": person_df, "City": city_df, "LIVES_IN": lives_in_df},
+    # )
+    # # obj: Algebraic = relationship.to_algebra(context)
+    # # print(obj)
+    
+    # import rich
+    # obj: Algebraic = relationship.to_algebra(context)
+    # rich.print(obj)
+    # print(obj.to_pandas(context))
